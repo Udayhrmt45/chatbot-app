51aabaacc4620462f875ddbd6dc72da715cd7f4e
Udayhrmt45 <hiremathuday68@gmail.com>
2024-11-10 16:50:03 +0530

    Initial commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Sidebar from './components/sidebar/sidebar';
+const App = () => {
+  return (
+    <><Sidebar/></>
+  )
+}
+
+export default App
---
+++ my-react-app/src/components/sidebar/sidebar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './sidebar.css'
+import {assets} from '../../assets/assets'
+const Sidebar = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Sidebar
